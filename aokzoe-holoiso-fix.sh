69a066603e96e82c43b408cdda2a3beb7cf21b67
NikhilSaini38 <nkhlsn38@gmail.com>
2022-11-15 14:43:39 +1100

    Update aokzoe-holoiso-fix.sh

--- aokzoe-holoiso-fix.sh
+++ aokzoe-holoiso-fix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl https://github.com/theVakhovskeIsTaken/grub-holoiso-pkgbuild/raw/6d65b01e9110ee0da8209e0b662dce8b79231e68/grub.default > /etc/default/grub
+curl v https://raw.githubusercontent.com/theVakhovskeIsTaken/grub-holoiso-pkgbuild/main/grub.default > /etc/default/grub
 holoiso-grub-update
