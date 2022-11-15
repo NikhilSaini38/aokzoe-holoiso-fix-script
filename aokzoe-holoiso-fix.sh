cd11a8519d8522ab9d8e7f3ccf215b518f10712e
NikhilSaini38 <nkhlsn38@gmail.com>
2022-11-15 16:08:07 +1100

    Update aokzoe-holoiso-fix.sh

--- aokzoe-holoiso-fix.sh
+++ aokzoe-holoiso-fix.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl v https://raw.githubusercontent.com/theVakhovskeIsTaken/grub-holoiso-pkgbuild/main/grub.default > /etc/default/grub
+curl https://raw.githubusercontent.com/theVakhovskeIsTaken/grub-holoiso-pkgbuild/main/grub.default > /etc/default/grub
+curl https://raw.githubusercontent.com/NikhilSaini38/aokzoe-holoiso-fix-script/main/gamescope-session > /usr/bin/gamescope-session
 holoiso-grub-update
