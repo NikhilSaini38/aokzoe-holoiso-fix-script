faa355691b9202abd3681ea1878dd551fbc817f0
NikhilSaini38 <nkhlsn38@gmail.com>
2022-11-15 19:45:32 +1100

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sudo bash /home/liveuser/Desktop/install.desktop
 sudo arch-chroot /mnt sh -c "curl -o- https://raw.githubusercontent.com/NikhilSaini38/aokzoe-holoiso-fix-script/main/aokzoe-holoiso-fix.sh | bash"
